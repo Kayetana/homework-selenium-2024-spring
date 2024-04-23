80652ed54704fedfa8a641b35b4a0851caf56b1f
Kayetana <Ane4kaGr@mail.ru>
2024-04-23 22:11:55 +0300

    initial

---
+++ hw/code/test_registration.py
@@ -0,0 +1,9 @@
+from base_case import BaseCase
+
+import time
+
+
+class TestRegistration(BaseCase):
+    def test_first(self):
+        time.sleep(5)
+        assert 1 == 1
---
+++ hw/code/ui/pages/base_page.py
@@ -0,0 +1,44 @@
+import time
+
+import allure
+from selenium.webdriver.remote.webelement import WebElement
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from ui.locators import locators
+
+
+class PageNotOpenedException(Exception):
+    pass
+
+
+class BasePage(object):
+    locators = locators.BasePageLocators()
+    url = 'https://ads.vk.com/'
+
+    def is_opened(self, timeout=15):
+        started = time.time()
+        while time.time() - started < timeout:
+            if self.driver.current_url == self.url:
+                return True
+        raise PageNotOpenedException(f'{self.url} did not open in {timeout} sec, current url {self.driver.current_url}')
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.is_opened()
+
+    def wait(self, timeout=None):
+        if timeout is None:
+            timeout = 5
+        return WebDriverWait(self.driver, timeout=timeout)
+
+    def find(self, locator, timeout=None) -> WebElement:
+        return self.wait(timeout).until(EC.presence_of_element_located(locator))
+
+    @allure.step('Click')
+    def click(self, locator, timeout=None) -> WebElement:
+        self.find(locator, timeout=timeout)
+        elem = self.wait(timeout).until(EC.element_to_be_clickable(locator))
+        elem.click()
+
+        return elem
